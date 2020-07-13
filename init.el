b480809e8df42863732c4958f6deba5715ceb167
qikun1990 <qikun1990@gmail.com>
2020-07-13 21:39:24 +0800

    安装org-pomodoro

--- init.el
+++ init.el
@@ -111,7 +111,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(nyan-mode t)
- '(package-selected-packages (quote (evil undo-tree nyan-mode company))))
+ '(package-selected-packages (quote (org-pomodoro evil undo-tree nyan-mode company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
