8f365b8699022218f92031747da1c8bb0a7981c4
qikun <qikun1990@gmail.com>
2020-09-19 19:51:04 +0800

    Update init.el

--- init.el
+++ init.el
@@ -289,3 +289,5 @@ charset
 (global-set-key (kbd "C-c g") 'counsel-git)
 (global-set-key (kbd "C-c j") 'counsel-git-grep)
 (global-set-key (kbd "C-c k") 'counsel-ag)
+;; 关闭自动生成的备份文件
+(setq make-backup-files nil)
